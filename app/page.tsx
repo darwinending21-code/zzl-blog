4bfe000ed76728ef7dcec55b15834704d288a48d
洪岩强 <9706597+hong-yanqiang@user.noreply.gitee.com>
2026-04-14 22:45:44 +0800

    22

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
+import AboutPage from "./about/page";
 export default function Home() {
-  const [isShow, setIsShow] = useState(false);
 
-  return <h1>待开发...</h1>;
+  return <AboutPage/>
 }
